c0059a9261980d7e4af6244711dc2bbaf8279ee0
Steven Williams <onomou@gmail.com>
2024-01-30 23:40:34 -0500

    Settings page enhancements

--- static/js/settings.js
+++ static/js/settings.js
@@ -1,20 +1,23 @@
 $(document).ready(function () {
-    $("#section-selection").on('change', function() {
-    console.log('target')
+    
+    $("#section-selection").on("change", function() {
+    // console.log("target")
     var target = $(this).val();
     var valid_ids = $("#hidden > div").map(function() {return this.id}).get()
-    if (valid_ids.includes(target)) {
-        var selected_options = $("#" + target)
-        console.log(target)
-        console.log(selected_options)
+    var selected_options = $("#hidden").find('div[data-section="' + target + '"]');
+    // if (valid_ids.includes(target)) {
+    if (selected_options.length > 0) {
         $("#section-settings").html(selected_options.html())
+        // var selected_options = $("#" + target)
+        // console.log(target)
+        // console.log(selected_options)
     } else {
         create_new_section()
     }
   });
-    $('.section-selection').trigger('change');
+    $("#section-selection").trigger("change");
     var max_size = Math.min(8, $("#section-selection option").length)
-    $("#section-selection").prop('size',max_size)
+    $("#section-selection").prop("size",max_size)
 })
 
 function create_new_section() {
